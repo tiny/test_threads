6f8f24cdd61a7a40c9f6c7544fb61c97af0501fc
Robert McInnis <tiny@solidice.com>
2025-01-25 05:41:57 -0500

    added basic components

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test_threads
 producer consumer thread sample
+
+![](notes/note_25jan2025a.png)
---
+++ src/main_testthreads.cpp
@@ -0,0 +1,10 @@
+/*
+  main_testthreads.cpp
+*/
+#include <stdio.h>
+
+int main( int argc, const char *argv[] )
+{
+    printf( "hello world\n" ) ;
+    return 0 ;
+} // :: main
